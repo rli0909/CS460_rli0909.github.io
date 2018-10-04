35093fa5306d7e35672793b57d216e2d65badcc6
Runnan Li <rli12@wou.edu>
2018-10-04 11:29:35 -0700

    problem fixed link working

--- homework2/dic.js
+++ homework2/dic.js
@@ -1,5 +1,5 @@
 
-var app = function(){
+var app = function () {
 
     var ob = {
 
@@ -7,7 +7,7 @@ var app = function(){
 
     var inputdata;
     var data;
-    var result;
+    var dicresult;
     var resultcreated = true;
     var font = {
         " ": ["░░░", "░░░", "░░░", "░░░", "░░░", "░░░"],
@@ -88,12 +88,23 @@ var app = function(){
         "%": ["░░░░░░░░░", "░░▄░░░░▄░", "░▀▄▀░▄▀░░", "░░░▄▀░▄▀▄", "░░▀░░░░▀░", "░░░░░░░░░"]
     };
 
-    ob.init=function(){
+
+    var inputlink;
+    var checkm;
+    var inbalidm;
+    var linkresult;
+    var linkCreated = true;
+    var reg = /[a-zA-Z0-9]+\-[a-zA-Z0-9]+\-[a-zA-Z0-9]+\-[a-zA-Z0-9]+\&InstanceId\=[a-zA-Z0-9]+-[a-zA-Z0-9]+\-[a-zA-Z0-9]+\-[a-zA-Z0-9]+\-[a-zA-Z0-9]+/;
+
+
+
+
+    ob.init = function () {
         inputdata = document.getElementById("bigfontinput");
-        $("#gobtn").click(function() {
+        $("#gobtn").click(function () {
             data = inputdata.value;
             var b = "";
-            var out = document.getElementById("out");
+            var outdic = document.getElementById("outdic");
             for (var j = 0; j < 6; j++) {
                 var s = "";
                 for (var i = 0; i < data.length; i++) {
@@ -103,34 +114,98 @@ var app = function(){
                 //console.log(s);
                 b += s + "░" + "\n";
             }
-            result = b;
-            out.value = result;
+            dicresult = b;
+            outdic.value = dicresult;
             resultcreated = true;
         });
 
 
-    $("#bigfontinput").focus(function () {
-        if (resultcreated) {
-            inputdata.value = '';
-            resultcreated = false;
-        }
-    });
+        $("#bigfontinput").focus(function () {
+            if (resultcreated) {
+                inputdata.value = '';
+                resultcreated = false;
+            }
+        });
 
-    $("#selectbtn").click(function () {
-        var text = document.getElementById("out");
-        //Event.preventDefault();
-        text.select();
-        document.execCommand("copy");
-        //alert("Copied");
-    });
+        $("#selectbtn").click(function () {
+            var text = document.getElementById("outdic");
+            //Event.preventDefault();
+            text.select();
+            document.execCommand("copy");
+            //alert("Copied");
+        });
+
+
+
+        inputlink = document.getElementById("datain");
+        checkm = document.getElementById("checkmark");
+        checkm.style.visibility = "hidden";
+        invalidm = document.getElementById("invalid");
+        invalidm.style.visibility = "hidden";
+
+        inputlink.onkeyup = onkeyup;
+        onkeyup = function(){
+            if (inputlink.value.search(reg) >= 0) {
+                var array = inputlink.value.split("=");
+                checkm.style.visibility = "visible";
+                invalidm.style.visibility = "hidden";
+                var outto = document.getElementById("outto");
+                var outLink = document.getElementById("outLink");
+
+                var part1 = array[1].split("&")[0];
+                var part2 = array[2];
+
+                linkresult = "https://www2.wou.edu/nora/eclipse.auth2?pdata=" + part1 + "," + part2;
+
+                outto.innerHTML = linkresult;
+                outLink.href = linkresult;
+            } else {
+                invalidm.style.visibility = "visible";
+                checkm.style.visibility = "hidden";
+                document.getElementById("mypop").style.visibility = "visible";
+                document.getElementById("gobutton").disabled = true;
+                document.getElementById("gobutton").style.visibility = "hidden";
+            }
+        };
+
+        $("#datain").focus(function () {
+            if (linkCreated) {
+                inputlink.value = '';
+                linkCreated = false;
+            }
+        });
+
+        $("#gobutton").click(function () {
+            if (inputlink.value.search(reg) >= 0) {
+                var array = inputlink.value.split("=");
+                checkm.style.visibility = "visible";
+                invalidm.style.visibility = "hidden";
+                var outto = document.getElementById("outto");
+                var outLink = document.getElementById("outLink");
+
+                var part1 = array[1].split("&")[0];
+                var part2 = array[2];
+
+                liinkresult = "https://www2.wou.edu/nora/eclipse.auth2?pdata=" + part1 + "," + part2;
+
+                outto.innerHTML = linkresult;
+                outLink.href = linkresult;
+                linkCreated = true;
+            } else {
+                invalidm.style.visibility = "visible";
+                checkm.style.visibility = "hidden";
+                document.getElementById("mypop").style.visibility = "visible";
+                linkCreated = false;
+            }
+        });
+
     };
 
     return ob;
 }();
 
 
-   
 
 
 
-    
+
