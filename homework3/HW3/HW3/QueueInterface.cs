d2f4801f81e9f1622bb76a4a3bfaafdbfbd68a47
Runnan Li <rli12@wou.edu>
2018-10-09 16:59:45 -0700

    edit homework3 while on master, and try to switch to hw3, error message

---
+++ homework3/HW3/HW3/QueueInterface.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    /**
+     * A FIFO queue interface. This ADT is suitable for a singly 
+     * linked queue.
+     */
+    class QueueInterface<T>
+    {
+        /**
+         * Add an element to the rear of the queue
+         * 
+         * @return the element that was enqueued
+         */
+        
+
+    }
+}
