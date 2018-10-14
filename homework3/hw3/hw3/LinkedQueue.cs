c83d53f21e18e2a6c721d4ca4567e90ed892751d
Runnan Li <rli12@wou.edu>
2018-10-13 18:47:33 -0700

    fix default(T) problem

--- homework3/hw3/hw3/LinkedQueue.cs
+++ homework3/hw3/hw3/LinkedQueue.cs
@@ -8,20 +8,20 @@ namespace hw3
 {
     class LinkedQueue<T> : IQueue<T>
     {
-        private Node<T> front;
-        private Node<T> rear;
+        private Node<T> Front;
+        private Node<T> Rear;
 
         public LinkedQueue()
         {
-            front = null;
-            rear = null;
+            Front = null;
+            Rear = null;
         }
 
 
         public bool isEmpty()
         {
             
-            if (front == null && rear == null)
+            if (Front == null && Rear == null)
             {
                 return true;
             }
@@ -33,12 +33,41 @@ namespace hw3
 
         public T pop()
         {
-            throw new NotImplementedException();
+            T tmp = default(T);
+
+            if (isEmpty())
+            {
+                throw new QueueUnderflowException("The queue was empty when pop was invoked.");
+            }
+            else if (Front == Rear)
+            {
+                tmp = Front.data;
+                Front = null;
+                Rear = null;
+            }
+            else
+            {
+                tmp = Front.data;
+                Front = Front.next;
+            }
+            return tmp;
         }
 
         public T push(T element)
         {
-            throw new NotImplementedException();
+            if (element == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            if (isEmpty())
+            {
+                Node<T> tmp = new Node<T>(element, null);
+                Rear.next = tmp;
+                Rear = tmp;
+            }
+
+            return element;
         }
     }
 }
