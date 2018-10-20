1d35e8efedf313904b7690ed22881ce5704f4621
Runnan Li <rli12@wou.edu>
2018-10-20 01:20:44 -0700

    Converter done, working on ColorChooser

--- homework4/HW4/HW4/Controllers/HomeController.cs
+++ homework4/HW4/HW4/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -34,17 +35,49 @@ namespace HW4.Controllers
             string mile = Request.QueryString["miles"];
             string unit = Request.QueryString["unit"];
 
-            
-            //int? miles = int.Parse(mile);
-            //int? units = int.Parse(unit);
+            float miles = 0.0f;
+            float units = 0.0f;
 
-            ViewBag.Message = mile + "&&&&&" + unit;
+            if (mile != null && unit != null)
+            {
+                miles = float.Parse(mile);
+                units = float.Parse(unit);
+            }
+
+            float result = miles * units;
+
+            if (unit == "1609344")
+            {
+                ViewBag.Message = miles + " miles " + " is equals to " + result + " millimeters";
+            }
+            else if (unit == "160934.4")
+            {
+                ViewBag.Message = miles + " miles " + " is equals to " + result + " centimeters";
+            }
+            else if (unit == "1609.344")
+            {
+                ViewBag.Message = miles + " miles " + " is equals to " + result + " meters";
+            }
+            else if (unit == "1.609344")
+            {
+                ViewBag.Message = miles + " miles " + " is equals to " + result + " kilometers";
+            }
+
             return View();
         }
 
-
         public ActionResult ColorChooser()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ColorChooser(Color color1, Color color2)
+        {
+
+            string htmlColor1 = ColorTranslator.ToHtml(color1);
+            string htmlColor2 = ColorTranslator.ToHtml(color2);
+
             return View();
         }
     }
