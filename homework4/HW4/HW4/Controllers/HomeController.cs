5da93db248b59c712c3894d155a17520456ec1df
Runnan Li <rli12@wou.edu>
2018-10-20 21:12:13 -0700

    colorChooser working

--- homework4/HW4/HW4/Controllers/HomeController.cs
+++ homework4/HW4/HW4/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -72,18 +73,70 @@ namespace HW4.Controllers
         }
 
         [HttpPost]
-        public ActionResult ColorChooser(int? id, int? size, string kind)
+        public ActionResult ColorChooser(string color1, string color2)
         {
-            
 
 
-<<<<<<< HEAD
-            ViewBag.mix = htmlColor1 + htmlColor2;
+            int a1 = ViewBag.color1 = ColorTranslator.FromHtml(color1).A;
+            int a2 = ViewBag.color2 = ColorTranslator.FromHtml(color2).A;
+            int mixA = a1 + a2;
+            if (mixA >= 255)
+            {
+                mixA = 255;
+            }
+
+
+            int red1 = ViewBag.color1 = ColorTranslator.FromHtml(color1).R;
+            int red2 = ViewBag.color1 = ColorTranslator.FromHtml(color2).R;
+
+            int mixRed = red1 + red2;
+            if (mixRed >= 255)
+            {
+                mixRed = 255;
+            }
+
+            int green1 = ViewBag.color1 = ColorTranslator.FromHtml(color1).G;
+            int green2 = ViewBag.color1 = ColorTranslator.FromHtml(color2).G;
+
+            int mixGreen = green1 + green2;
+            if (mixGreen >= 255)
+            {
+                mixGreen = 255;
+            }
+
+
+            int blue1 = ViewBag.color1 = ColorTranslator.FromHtml(color1).B;
+            int blue2 = ViewBag.color1 = ColorTranslator.FromHtml(color2).B;
+
+            int mixBlue = blue1 + blue2;
+            if (mixBlue >= 255)
+            {
+                mixBlue = 255;
+            }
+
+
+            //Get result color in ARGB structure
+            Color result = Color.FromArgb(mixA, mixRed, mixGreen, mixBlue);
+            string hexValue = ColorTranslator.ToHtml(result);
+            ViewBag.mix = hexValue;
+
+            //Convert value to ARGB
+            //int colorValue = result.ToArgb();
+            //ViewBag.mixValue = colorValue;
+
+
+            //draw first color block
+        
+
+
+
+
+            ViewBag.color1 = color1;  //return A value
+            ViewBag.color2 = color2;  //return B value
+
+
 
             return View();
-=======
-            
->>>>>>> 7d9d49500b944c434e420e6dd99ef553a61aaa63
         }
     }
 }
