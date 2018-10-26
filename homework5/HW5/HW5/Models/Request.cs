a8e3818fc58f75201e8e3b09567da16dadd4f12d
Runnan Li <rli12@wou.edu>
2018-10-26 12:18:50 -0700

    add model h5

---
+++ homework5/HW5/HW5/Models/Request.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HW5.Models
+{
+    public class Request
+    {
+        [Required]
+        [StringLength(20)]
+        public string FirstName { get; set; }
+
+        [StringLength(10)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string PhoneNum { get; set; }
+
+        [Required]
+        public string AptName{ get; set; }
+
+        [Required]
+        public int unit { get; set; }
+
+
+    }
+}
