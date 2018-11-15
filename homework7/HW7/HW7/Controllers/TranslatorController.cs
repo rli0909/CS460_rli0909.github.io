97e6f1f8c6d9fef19888d8062d4c1eea38967d30
Runnan Li <rli12@wou.edu>
2018-11-15 13:19:51 -0600

    try to call server from controller

--- homework7/HW7/HW7/Controllers/TranslatorController.cs
+++ homework7/HW7/HW7/Controllers/TranslatorController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +9,13 @@ namespace HW7.Controllers
 {
     public class TranslatorController : Controller
     {
+
+        // client object
+       // static HttpClient client = new HttpClient();
+
+       // var api = "http://api.giphy.com/v1/gifs/translate?";
+       // var apiKey = "&api_key=gMrqiddCIe001wwIeLKG0llVIXfOKmh9";
+
         //GET: HOME
         public ActionResult Index()
         {
@@ -28,7 +36,14 @@ namespace HW7.Controllers
 
             // AJAX call to server
             // How to do AJAX call in controllor
-          
+            //var url = api + apiKey + "&s=" + id;
+
+            //response = client.GetAsync(url);
+           // if (response.IsSuccessStatusCode)
+           // {
+
+          //  }
+
 
             return Json(data, JsonRequestBehavior.AllowGet );
         }
