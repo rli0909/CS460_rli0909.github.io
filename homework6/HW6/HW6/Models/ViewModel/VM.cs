ba479c8a5deca82d11facb2557790f0a85a8f13f
Runnan Li <rli12@wou.edu>
2018-11-19 16:19:41 -0800

    h6 viewmodel

---
+++ homework6/HW6/HW6/Models/ViewModel/VM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW6.Models.ViewModel
+{
+    public class VM
+    {
+        public Person Person { get; set; }
+        public Customer Customer { get; set; }
+        public List<InvoiceLine> InvoiceLine { get; set; }
+    }
+}
